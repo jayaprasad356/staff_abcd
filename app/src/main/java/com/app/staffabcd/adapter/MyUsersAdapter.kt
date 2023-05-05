d9881e684571e9a4a6e3f519eded57bbb949dac3
ajay <>
2023-05-05 15:47:20 +0530

    my user chat

--- app/src/main/java/com/app/staffabcd/adapter/MyUsersAdapter.kt
+++ app/src/main/java/com/app/staffabcd/adapter/MyUsersAdapter.kt
@@ -1,10 +1,17 @@
 package com.app.staffabcd.adapter
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.PopupMenu
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 import com.app.staffabcd.R
@@ -36,6 +43,9 @@ class MyUsersAdapter(val activity: Activity, users: ArrayList<Users>) :
         holder.tvWokedDays.text="Worked Days: "+users1.worked_days
         holder.tvTotalreferrals.text="No. of refers: "+users1.total_referrals
         holder.tvTotalreferrals.text="No. of refers: "+users1.total_referrals
+        holder.btnChat.setOnClickListener {view ->
+            showPopupMenu(view,users1.mobile)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -49,6 +59,7 @@ class MyUsersAdapter(val activity: Activity, users: ArrayList<Users>) :
         val tvMobile: TextView
         val tvWokedDays: TextView
         val tvTotalreferrals: TextView
+        val btnChat: Button
         init {
             tvName = itemView.findViewById<TextView>(R.id.tvName)
             tvReferCode = itemView.findViewById<TextView>(R.id.tvReferCode)
@@ -56,6 +67,45 @@ class MyUsersAdapter(val activity: Activity, users: ArrayList<Users>) :
             tvMobile = itemView.findViewById<TextView>(R.id.tvMobile)
             tvWokedDays = itemView.findViewById<TextView>(R.id.tvWokedDays)
             tvTotalreferrals = itemView.findViewById<TextView>(R.id.tvTotalreferrals)
+            btnChat = itemView.findViewById<Button>(R.id.btnChat)
+
         }
     }
+    private fun showPopupMenu(view: View, mobile: String?) {
+        val popup = PopupMenu(activity, view)
+        popup.inflate(R.menu.whatsapp_menu)
+        popup.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.whatsapp -> sendMsgToWhatsapp(mobile,"com.whatsapp")
+                R.id.business_whatsapp -> sendMsgToWhatsapp(mobile,"com.whatsapp.w4b")
+            }
+            true
+        }
+        popup.show()
+    }
+    private fun sendMsgToWhatsapp(mobile: String?, packageName: String) {
+
+        val phoneNumber = "+91$mobile"
+        val message = "Hi, How are you?" // Replace with the message you want to send
+// Replace with the phone number you want to navigate to in WhatsApp
+
+        val packageManager: PackageManager =
+            activity.packageManager// Call packageManager on the activity instance
+        val i = Intent(Intent.ACTION_VIEW)
+
+        try {
+            val url =
+                "https://api.whatsapp.com/send?phone=$phoneNumber&text=$message" // Include the message in the URL
+
+            i.setPackage(packageName)
+            i.data = Uri.parse(url)
+
+            activity.startActivity(i)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(activity, "WhatsApp not installed", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
 }
